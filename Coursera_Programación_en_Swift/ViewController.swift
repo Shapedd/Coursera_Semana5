80efe30908bc63f1285468ff94acc1a45d628173
Carlos Sanchez San Segundo <carlos.sanchez-sansegundo@prosegur.com>
2016-01-07 23:29:13 +0100

    Initial Commit

---
+++ Coursera_Programación_en_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coursera_Programación_en_Swift
+//
+//  Created by Carlos Sanchez San Segundo on 7/1/16.
+//  Copyright © 2016 Carlos Sanchez San Segundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
