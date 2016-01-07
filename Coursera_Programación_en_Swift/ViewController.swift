a7691194d41a1525cb9882c21305601345042d31
Carlos Sanchez San Segundo <carlos.sanchez-sansegundo@prosegur.com>
2016-01-07 23:57:11 +0100

    Hamburguesas por el mundo!

--- Coursera_Programación_en_Swift/ViewController.swift
+++ Coursera_Programación_en_Swift/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var Pais : UILabel!
+    let pais = ColeccionDePaises()
+    
+    @IBOutlet weak var Hamburguesa : UILabel!
+    let hamburguesa = ColeccionDeHamburguesas()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +27,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    @IBAction func PulsaBoton() {
+        Pais.text = pais.obtenPais()
+        Hamburguesa.text = hamburguesa.obtenHamburguesa()
     }
 
 
